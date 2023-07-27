f5c0b05abf22c8c540e75fb1cf9b98fc2206e62e
ebruekin <54847953+ebruekin@users.noreply.github.com>
2023-07-27 21:16:51 +0300

    Update Program.cs

--- APITesting/Program.cs
+++ APITesting/Program.cs
@@ -1,29 +1,39 @@
 using APITesting.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 // Custom Services (DI)
 builder.Services.AddScoped<IBeerServices, BeerServices>();
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        c.RoutePrefix = ""; // Bu satırı ekleyerek root adresi ile Swagger arayüzüne erişebilirsiniz.
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
